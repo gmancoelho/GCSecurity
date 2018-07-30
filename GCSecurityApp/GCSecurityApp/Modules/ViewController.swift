579fbabbb99efa6b0c1e53324c9b9ca239c9a5dc
Guilherme <guizin.coelho@gmail.com>
2018-07-30 17:20:42 -0300

    Project init

---
+++ GCSecurityApp/GCSecurityApp/Modules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCSecurityApp
+//
+//  Created by Guilherme Coelho on 30/07/18.
+//  Copyright © 2018 DevForFun_Guilherme_Coelho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
